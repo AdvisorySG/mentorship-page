41a7213e015b307b82c94c1bbdeacd7ce203a69e
Ng Wei En <weien1292@gmail.com>
2020-04-09 21:21:16 +0800

    App.js: Shift mentorData to separate file.

--- src/App.js
+++ src/App.js
@@ -4,34 +4,9 @@ import ProfileCard from "./components/profile-card";
 import Header from "./components/header";
 import ProfileModal from "./components/profile-modal";
 
-function App() {
-  const mentorData = [
-    {
-      id: 0,
-      name: "Jane Doe",
-      role: "MFA Student",
-      organization: "SVA, New York City",
-      school: "UC Irvine",
-      course_of_study: "Fine Arts",
-      full_bio:
-        "Jane Doe is an MFA student at the School of Visual Arts, New York City. She graduated from UC Irvine in 2016, and works predominantly in Photography. Longer lines of text are usually full of extraneous words very much like this one indeed. I need this thing to be justified omg.",
-      image_url:
-        "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9",
-    },
-    {
-      id: 1,
-      name: "Peter Pan",
-      role: "Wizard",
-      organization: "Asteroid Belt Infinity Enterprises",
-      school: "Waverly Place",
-      course_of_study: "Meteor Alchemy",
-      full_bio:
-        "Peter Pan is a meteor conjurer at the Association of Waverly Place, 0:0:1132:124932 Solar System 33148293.",
-      image_url:
-        "https://i.pinimg.com/originals/42/9e/9f/429e9f32eb7d0213342a4e447e6e93ae.jpg",
-    },
-  ];
+import mentorData from "./mentorData.json";
 
+function App() {
   const [modalVisible, setModalVisible] = useState(false);
   const [modalId, setModalId] = useState(null);
 
@@ -53,12 +28,12 @@ function App() {
         <ProfileModal
           id={modalId}
           onClose={closeHandler}
-          {...mentorData[modalId]}
+          {...mentorData.data[modalId]}
         />
       )}
 
       <div className="profile-cards">
-        {mentorData.map((mentor) => (
+        {mentorData.data.map((mentor) => (
           <ProfileCard
             key={mentor.id}
             img_url={mentor.image_url}
