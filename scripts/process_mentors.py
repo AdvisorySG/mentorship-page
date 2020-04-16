f8ab0de7767957ea4d8a1f1e904121d32d83d913
Ng Wei En <weien1292@gmail.com>
2020-04-16 22:11:54 +0800

    Mentors: Add a sample set of mentors (#43)
    Mentors: Add sample list along with all mentor images

---
+++ scripts/process_mentors.py
@@ -0,0 +1,24 @@
+import csv
+import json
+import sys
+
+with open(sys.argv[1], newline='') as f:
+    lines = csv.reader(f, delimiter=',', quotechar='"')
+    mentors = []
+    for row in lines:
+        row = [x.strip() for x in row]
+        if row[0] == "":
+            continue
+
+        mentors.append({
+            "name": row[0],
+            "role": row[1],
+            "organization": row[2],
+            "school": row[3],
+            "courseOfStudy": row[4],
+            "fullBio": row[5],
+            "imageUrl": "/profile_images/{}.jpg".format(
+                row[0].replace(" ", "")
+            )
+        })
+    print(json.dumps({"mentors": mentors})) 
