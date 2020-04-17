94d78237534678d0d0e7cb9c13df9fdb028b6ab0
Ryan Tan <ryan.rtjj@gmail.com>
2020-04-16 23:34:54 -0700

    metatags attempt 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
+import DocumentMeta from "react-document-meta";
 
 import Header from "./components/header";
 import ProfileCard from "./components/profile-card";
 import ProfileModal from "./components/profile-modal";
 import { mentors } from "./mentors.json";
+import { advisoryMentorshipLogo } from "./assets";
 
 import "./App.css";
 
 const visibleMentorIds = mentors.map((mentor, index) => index);
+const meta = {
+  title: "Advisory Mentorship",
+  description: "The Advisory Mentorship Program",
+  canonical: "mentorship.advisory.sg",
+  meta: {
+    charset: "utf-8",
+    name: {
+      keywords: "advisory,mentorship,program,professional,industry,education",
+    },
+    property: {
+      "og:title:": "The Advisory Mentorship Program",
+      "og:url": "https//mentorship.advisory.sg",
+      "og:image": { advisoryMentorshipLogo },
+      "og:description": "The Advisory Mentorship Program",
+
+      "twitter:title": "The Advisory Mentorship Program",
+      "twitter:description": "The Advisory Mentorship Program",
+      "twitter:image": { advisoryMentorshipLogo },
+    },
+  },
+};
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -23,25 +46,27 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Header />
+    <DocumentMeta {...meta}>
+      <div className="container">
+        <Header />
 
-      <ProfileModal
-        isOpen={isModalOpen}
-        mentor={mentors[activeMentorId]}
-        onClose={closeHandler}
-      />
+        <ProfileModal
+          isOpen={isModalOpen}
+          mentor={mentors[activeMentorId]}
+          onClose={closeHandler}
+        />
 
-      <div className="card-container">
-        {visibleMentorIds.map((mentorId) => (
-          <ProfileCard
-            key={mentorId}
-            mentor={mentors[mentorId]}
-            onReadMore={() => readMoreHandler(mentorId)}
-          />
-        ))}
+        <div className="card-container">
+          {visibleMentorIds.map((mentorId) => (
+            <ProfileCard
+              key={mentorId}
+              mentor={mentors[mentorId]}
+              onReadMore={() => readMoreHandler(mentorId)}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </DocumentMeta>
   );
 }
 
