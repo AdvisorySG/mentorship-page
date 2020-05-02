f7ceb73f7f7af468248fbcbba7d846d616229f68
Ng Wei En <weien1292@gmail.com>
2020-05-02 22:17:38 +0800

    Mentors: Change mentors array to object indexed by ID

--- scripts/process_mentors.py
+++ scripts/process_mentors.py
@@ -32,7 +32,7 @@ IMG_SIZES = {
 @click.argument('im_src', type=click.Path(file_okay=False, dir_okay=True))
 @click.argument('im_dst', type=click.Path(file_okay=False, dir_okay=True))
 def main(csv_src, im_src, im_dst):
-    def process_image(im, im_name):
+    def process_image(im, mentor_id):
         im_hash = hashlib.sha256(im.tobytes()).hexdigest()[:7]
         im_paths = {}
         for size_name, size in IMG_SIZES.items():
@@ -40,7 +40,7 @@ def main(csv_src, im_src, im_dst):
 
             im2 = im.resize(size)
             im2_dir = Path('{}x{}'.format(w, h))
-            im2_fn = Path('{}.{}.jpg'.format(im_name, im_hash))
+            im2_fn = Path('{}.{}.jpg'.format(mentor_id, im_hash))
 
             Path(im_dst, im2_dir).mkdir(parents=True, exist_ok=True)
             im2.save(Path(im_dst, im2_dir, im2_fn), optimize=True, progressive=True)
@@ -56,25 +56,19 @@ def main(csv_src, im_src, im_dst):
         for i, field in enumerate(CSV_FIELDS):
             if field:
                 mentor[field] = row[i]
-        im_name = mentor['firstNameAndLastName'].replace(' ', '').replace('(', '').replace(')', '')
+        mentor_id = mentor['firstNameAndLastName'].replace(' ', '').replace('(', '').replace(')', '')
 
-        with Image.open(Path(im_src, '{}.jpg'.format(im_name))) as im:
-            im_paths = process_image(im, im_name)
+        with Image.open(Path(im_src, '{}.jpg'.format(mentor_id))) as im:
+            im_paths = process_image(im, mentor_id)
             for size_name, im_path in im_paths.items():
                 mentor['{}ImageUrl'.format(size_name)] = '/profile_images/{}'.format(im_path)
 
-        return mentor
+        return (mentor_id, mentor)
 
     with open(csv_src, newline='', encoding='utf-8') as f:
         lines = csv.reader(f, delimiter=',', quotechar='"')
-        mentors = (process_row(row) for row in lines if process_row(row))
-        print(json.dumps(
-            {
-                'mentors': sorted(mentors, key=lambda m: m['name'])
-            },
-            indent=4,
-            sort_keys=True
-        ))
+        mentors = dict(process_row(row) for row in lines if process_row(row))
+        print(json.dumps({'mentors': mentors}, indent=4, sort_keys=True))
 
 if __name__ == '__main__':
     main()
