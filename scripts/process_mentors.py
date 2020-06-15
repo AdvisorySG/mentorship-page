81da0403d3a8c05c364425ad57462dcd665bde3e
Ng Wei En <weien1292@gmail.com>
2020-06-15 23:27:16 +0800

    Mentors: Update script to use an image filename column
    As this change is not backwards compatible, the existing CSV files have been
    changed into an 'archival' format that can be easily reorganised.

--- scripts/process_mentors.py
+++ scripts/process_mentors.py
@@ -9,18 +9,22 @@ import click
 import imagehash
 
 CSV_FIELDS = [
+    'name',
+    None,
+    None,
+    None,
+    None,
+    None,
+    'imageFilename',
     None,
     None,
     'fullBio',
     None,
     None,
-    None,
     'role',
     'organization',
     'school',
-    'courseOfStudy',
-    'firstNameAndLastName',
-    'name'
+    'courseOfStudy'
 ]
 
 IMG_SIZES = {
@@ -40,13 +44,16 @@ def main(csv_src, im_src, im_dst):
         if not csv_fn.is_file():
             break
 
-        def process_image(im, mentor_id, im_hash):
+        def process_image(im, im_fn, im_hash):
             im_paths = {}
+            im_fn_split = im_fn.split('.')
+            assert len(im_fn_split) > 1
+
             for size_name, size in IMG_SIZES.items():
                 w, h = size
                 im2 = im.resize(size)
                 im2_dir = Path('{}x{}'.format(w, h))
-                im2_fn = Path('{}.{}.jpg'.format(mentor_id, im_hash))
+                im2_fn = Path('{}.{}.{}'.format(im_fn_split[:-1], im_hash, im_fn_split[-1]))
 
                 Path(im_dst, im2_dir).mkdir(parents=True, exist_ok=True)
                 im2.save(Path(im_dst, im2_dir, im2_fn), optimize=True, progressive=True)
@@ -63,16 +70,15 @@ def main(csv_src, im_src, im_dst):
             for i, field in enumerate(CSV_FIELDS):
                 if field:
                     mentor[field] = row[i]
-            mentor_id = mentor['firstNameAndLastName'].replace(' ', '').replace('(', '').replace(')', '')
-
-            im_fn = Path(im_src, '{}.jpg'.format(mentor_id))
-            with Image.open(im_fn) as im:
+            
+            im_fn = mentor['imageFilename']
+            with Image.open(Path(im_src, im_fn)) as im:
                 im_hash = str(imagehash.average_hash(im))[:7]
-                im_paths = process_image(im, mentor_id, im_hash)
+                im_paths = process_image(im, im_fn, im_hash)
                 for size_name, im_path in im_paths.items():
                     mentor['{}ImageUrl'.format(size_name)] = '/profile_images/{}'.format(im_path)
 
-            return (mentor_id, mentor)
+            return (mentor['name'].replace(' ', '').replace('(', '').replace(')', ''), mentor)
 
         with open(csv_fn, newline='', encoding='utf-8') as f:
             lines = csv.reader(f, delimiter=',', quotechar='"')
