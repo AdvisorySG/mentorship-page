8111c164fbcdfd9ce6fe3fa0a3acab5d4f9748e2
= <=>
2021-03-01 16:00:35 +0800

    Asynchronously load all records; imperfect solution for now, but all data does get loaded

--- src/App.js
+++ src/App.js
@@ -19,7 +19,17 @@ const setHash = (hash) => window.history.replaceState({}, "", `#${hash}`);
 // Used to convert between wave and tab indices.
 const convertIndex = (index) => waves.length - 1 - index;
 
+// ISSUE: Offset values get refreshed at some time interval
+// TO SOLVE: (i) Offset refresh interval; (ii) A way to extract offset value to put in GET url
+const OFFSET = [
+  "",
+  "itrvU8v1zx0cDYcD5/rec4Ss3qkSPhm1tGw",
+  "itrvU8v1zx0cDYcD5/recfrILxJNtmE4z37",
+];
+
 function App() {
+  const [apiData, setApiData] = useState([]);
+
   const [waveIndex, setWaveIndex] = useState(waves.length - 1);
   const activateTab = (tabIndex) => setWaveIndex(convertIndex(tabIndex));
 
@@ -32,15 +42,32 @@ function App() {
 
   // Load Airtable data once on each app render
   useEffect(() => {
-    const loadData = async () => {
-      const response = await axios.get(
-        "https://d3f3pna2dcek92.cloudfront.net/"
-      );
-      console.log(JSON.stringify(response.data.records));
+    // Add each record into apiData array individually (can't think of better method for now)
+    const addData = (newData) => {
+      for (let i of newData) {
+        setApiData((apiData) => [...apiData, i]);
+      }
     };
+
+    // API call using for each OFFSET value, followed by addition into apiData
+    const loadData = async () => {
+      for (let i of OFFSET) {
+        const response = await axios.get(
+          `https://d3f3pna2dcek92.cloudfront.net/?sort%5B0%5D%5Bfield%5D=Name&offset=${encodeURIComponent(
+            i
+          )}`
+        );
+        addData(response.data.records);
+      }
+    };
+
     loadData();
-    // Do something with response here
   }, []);
+
+  // console.log apiData upon change in apiData
+  useEffect(() => {
+    (() => console.log(apiData))();
+  }, [apiData]);
 
   useEffect(() => {
     // Checks hash and ensures that any modal with a corresponding name is open.
