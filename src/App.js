c76c3782e3771eae954212455b5127027c155185
= <=>
2021-03-07 17:13:42 +0800

    Shifted mentors-loading process into script

--- src/App.js
+++ src/App.js
@@ -19,14 +19,6 @@ const setHash = (hash) => window.history.replaceState({}, "", `#${hash}`);
 // Used to convert between wave and tab indices.
 const convertIndex = (index) => waves.length - 1 - index;
 
-// ISSUE: Offset values get refreshed at some time interval
-// TO SOLVE: (i) Offset refresh interval; (ii) A way to extract offset value to put in GET url
-const OFFSET = [
-  "",
-  "itrvU8v1zx0cDYcD5/rec4Ss3qkSPhm1tGw",
-  "itrvU8v1zx0cDYcD5/recfrILxJNtmE4z37",
-];
-
 function App() {
   const [apiData, setApiData] = useState([]);
 
@@ -39,35 +31,6 @@ function App() {
     setActiveMentorId(mentorId);
     setIsModalOpen(true);
   };
-
-  // Load Airtable data once on each app render
-  useEffect(() => {
-    // Add each record into apiData array individually (can't think of better method for now)
-    const addData = (newData) => {
-      for (let i of newData) {
-        setApiData((apiData) => [...apiData, i]);
-      }
-    };
-
-    // API call using for each OFFSET value, followed by addition into apiData
-    const loadData = async () => {
-      for (let i of OFFSET) {
-        const response = await axios.get(
-          `https://d3f3pna2dcek92.cloudfront.net/?sort%5B0%5D%5Bfield%5D=Name&offset=${encodeURIComponent(
-            i
-          )}`
-        );
-        addData(response.data.records);
-      }
-    };
-
-    loadData();
-  }, []);
-
-  // console.log apiData upon change in apiData
-  useEffect(() => {
-    (() => console.log(apiData))();
-  }, [apiData]);
 
   useEffect(() => {
     // Checks hash and ensures that any modal with a corresponding name is open.
