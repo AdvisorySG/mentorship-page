86de413c30b057d1d053f638e75b526a9dffcb0d
Ng Wei En <weien1292@gmail.com>
2021-03-20 12:57:05 +0800

    prettier formatting

--- scripts/load_mentors.js
+++ scripts/load_mentors.js
@@ -7,20 +7,21 @@ const client = new AWS.SecretsManager({ region: region });
 exports.handler = async (event) => {
   let mentors = [];
 
-  const apiKey = await client.getSecretValue({ SecretId: secretName })
+  const apiKey = await client
+    .getSecretValue({ SecretId: secretName })
     .promise()
-    .then(data => JSON.parse(data.SecretString)["APIKey"]);
+    .then((data) => JSON.parse(data.SecretString)["APIKey"]);
   const base = new Airtable({ apiKey }).base("appDfSlmYKDyuAj51");
-  
+
   await base("4 Tech")
     .select({ sort: [{ field: "First Name", direction: "asc" }] })
     .eachPage((records, fetchNextPage) => {
-      mentors = mentors.concat(records.map(record => record.fields));
+      mentors = mentors.concat(records.map((record) => record.fields));
       fetchNextPage();
     });
-  
+
   return {
-      statusCode: 200,
-      body: mentors
+    statusCode: 200,
+    body: mentors,
   };
 };
