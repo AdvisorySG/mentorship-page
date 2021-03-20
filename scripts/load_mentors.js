188bc63072f0c6c33d3ce083e0be07093b7a5f8f
Ng Wei En <weien1292@gmail.com>
2021-03-20 13:02:19 +0800

    use push instead of concat and reassignment

--- scripts/load_mentors.js
+++ scripts/load_mentors.js
@@ -2,11 +2,11 @@ const Airtable = require("airtable");
 const AWS = require("aws-sdk"),
   region = "ap-southeast-1",
   secretName = "airtable_api_key";
-const client = new AWS.SecretsManager({ region: region });
 
 exports.handler = async (event) => {
-  let mentors = [];
+  const mentors = [];
 
+  const client = new AWS.SecretsManager({ region: region });
   const apiKey = await client
     .getSecretValue({ SecretId: secretName })
     .promise()
@@ -16,7 +16,7 @@ exports.handler = async (event) => {
   await base("4 Tech")
     .select({ sort: [{ field: "First Name", direction: "asc" }] })
     .eachPage((records, fetchNextPage) => {
-      mentors = mentors.concat(records.map((record) => record.fields));
+      mentors.push(...records.map((record) => record.fields));
       fetchNextPage();
     });
 
