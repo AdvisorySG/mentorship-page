012300eda22f6c084d14c33941bcc18c2f70ffe6
Faisal <spectra46@live.com>
2021-05-22 21:49:20 +0800

    Adds Material UI, Incomplete Card View

--- src/components/profile-card.jsx
+++ src/components/profile-card.jsx
@@ -1,36 +1,63 @@
 import LazyLoad from "react-lazyload";
 import React from "react";
-
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+import Divider from '@material-ui/core/Divider';
 import "./profile-card.css";
 
-const ReadMore = ({ onReadMore }) => (
-  <div className="card-read-more-container">
-    <button className="card-read-more" onClick={onReadMore}>
-      Read More
-    </button>
-  </div>
-);
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 240,
+  },
+  media: {
+    height: 240,
+    width: 240,
+  },
+});
 
-const ProfileCard = ({ mentor, onReadMore }) => (
-  <div className="card">
-    <div className="card-image-region">
-      <LazyLoad height={160} offset={480} once>
-        <img
-          className="card-image"
-          src={process.env.PUBLIC_URL + mentor.thumbnailImageUrl}
-          alt={mentor.name}
+export default function ProfileCard({ mentor, onReadMore }) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea onClick={onReadMore}>
+        <CardMedia
+          className={classes.media}
+          image={process.env.PUBLIC_URL + mentor.thumbnailImageUrl}
+          title={mentor.name}
         />
-      </LazyLoad>
-    </div>
-    <div className="card-descriptors">
-      <div className="card-name">{mentor.name}</div>
-      {mentor.role && <div className="card-desc">{mentor.role}</div>}
-      {mentor.organisation && (
-        <div className="card-desc">{mentor.organisation}</div>
-      )}
-    </div>
-    <ReadMore onReadMore={onReadMore} />
-  </div>
-);
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+          {mentor.name}
+          </Typography>
 
-export default ProfileCard;
+          {/* if {mentor.role} && {mentor.organisation} { */}
+            <Typography variant="body2" color="textSecondary" component="p">
+              {mentor.role}, {mentor.organisation}
+            </Typography>
+          {/* } */}
+          <Divider />
+
+          <Chip
+            size="small"
+            label={mentor.industry}
+            color="primary"
+          />
+
+          {/* if {mentor.industry} { */}
+            {/* <Typography variant="body2" color="textSecondary" component="p">
+              Industry: {mentor.industry}  
+            </Typography> */}
+          {/* } */}
+          
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
