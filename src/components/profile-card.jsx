2164ab2cdaa4dbec4281166ce739b4e751a23735
Ng Wei En <weien1292@gmail.com>
2021-05-23 23:22:43 +0800

    make some fancy cosmetic changes

--- src/components/profile-card.jsx
+++ src/components/profile-card.jsx
@@ -1,13 +1,12 @@
 import LazyLoad from "react-lazyload";
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import Chip from '@material-ui/core/Chip';
-import Divider from '@material-ui/core/Divider';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Chip from "@material-ui/core/Chip";
 import "./profile-card.css";
 
 const useStyles = makeStyles({
@@ -18,6 +17,11 @@ const useStyles = makeStyles({
     height: 240,
     width: 240,
   },
+  chip: {
+    maxWidth: 200,
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  },
 });
 
 export default function ProfileCard({ mentor, onReadMore }) {
@@ -26,38 +30,32 @@ export default function ProfileCard({ mentor, onReadMore }) {
   return (
     <Card className={classes.root}>
       <CardActionArea onClick={onReadMore}>
-        <CardMedia
-          className={classes.media}
-          image={process.env.PUBLIC_URL + mentor.thumbnailImageUrl}
-          title={mentor.name}
-        />
+        <LazyLoad height={240} offset={720} once>
+          <CardMedia
+            className={classes.media}
+            image={process.env.PUBLIC_URL + mentor.thumbnailImageUrl}
+            title={mentor.name}
+          />
+        </LazyLoad>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-          {mentor.name}
+            {mentor.name}
           </Typography>
 
-          {/* if {mentor.role} && {mentor.organisation} { */}
-            <Typography variant="body2" color="textSecondary" component="p">
-              {mentor.role}, {mentor.organisation}
-            </Typography>
-          {/* } */}
-          <Divider />
-
           <Chip
+            className={classes.chip}
             size="small"
             label={mentor.industry}
             color="primary"
           />
 
-          {/* if {mentor.industry} { */}
-            {/* <Typography variant="body2" color="textSecondary" component="p">
-              Industry: {mentor.industry}  
-            </Typography> */}
-          {/* } */}
-          
+          {mentor.role && mentor.organisation && (
+            <Typography variant="body2" color="textSecondary" component="p">
+              {mentor.role}, {mentor.organisation}
+            </Typography>
+          )}
         </CardContent>
       </CardActionArea>
     </Card>
   );
 }
-
