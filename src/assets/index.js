5d18dfa685ebf082ab3102f9735573ee7f51b3ad
Ng Wei En <weien1292@gmail.com>
2021-05-24 23:19:39 +0800

    Replace Mentorship logo with online variant

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,2 @@
 export const advisoryLogo = `${process.env.PUBLIC_URL}/advisory-logo.png`;
-export const advisoryMentorshipLogo = `${process.env.PUBLIC_URL}/advisory-mentorship-logo.png`;
+export const advisoryMentorshipLogo = `${process.env.PUBLIC_URL}/advisory-mentorship-logo-online.png`;
