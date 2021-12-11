0d5d5933bebe6f7dfb610d41a13f44149b558503
Ng Wei En <weien1292@gmail.com>
2021-12-11 23:49:29 +0800

    Perform mentor postprocessing in load_mentors.js
    This simplifies the process of uploading documents onto Elastic App
    Search, as all fields are now lowercase and no redundant fields are
    present.

--- scripts/load_mentors.js
+++ scripts/load_mentors.js
@@ -1,31 +1,63 @@
 const Airtable = require("airtable");
-const AWS = require("aws-sdk"),
-  region = "ap-southeast-1",
-  secretName = "airtable_api_key";
+const AWS = require("aws-sdk");
+
+const AWS_REGION = "ap-southeast-1";
+const AWS_SECRET_NAME = "airtable_api_key";
+const PLACEHOLDER_THUMBNAIL_URL = "/mentor-thumbnail.png";
+const WAVE_INFO = [
+  { tableId: "4 Tech", name: "2021 Wave 1" },
+  { tableId: "5 Tech", name: "2021 Wave 2" },
+];
+
+const formatMentor = (id, waveId, fields) => ({
+  id,
+  course_of_study: fields["Course of Study"],
+  full_bio: fields.Biography,
+  full_image_url:
+    Array.isArray(fields.Photo) && fields.Photo.length > 0
+      ? fields.Photo[0].url
+      : PLACEHOLDER_THUMBNAIL_URL,
+  industries: [
+    fields["Industry 1"] ?? [],
+    fields["Industry 2"] ?? [],
+    fields["Industry 3"] ?? [],
+  ].flat(),
+  name: fields.Name,
+  organisation: fields.Organisation,
+  role: fields["Job Title"],
+  school: fields.School,
+  thumbnail_image_url:
+    Array.isArray(fields.Photo) &&
+    fields.Photo.length > 0 &&
+    fields.Photo[0].thumbnails
+      ? fields.Photo[0].thumbnails.large.url
+      : PLACEHOLDER_THUMBNAIL_URL,
+  wave_id: waveId,
+  wave_name: WAVE_INFO[waveId].name,
+});
 
 exports.handler = async (event) => {
-  const waveTables = ["4 Tech", "5 Tech"];
-  const waves = Array.from(waveTables, () => []);
+  const mentors = [];
 
-  const client = new AWS.SecretsManager({ region: region });
+  const client = new AWS.SecretsManager({ region: AWS_REGION });
   const apiKey = await client
-    .getSecretValue({ SecretId: secretName })
+    .getSecretValue({ SecretId: AWS_SECRET_NAME })
     .promise()
     .then((data) => JSON.parse(data.SecretString)["APIKey"]);
   const base = new Airtable({ apiKey }).base("appDfSlmYKDyuAj51");
 
   await Promise.all(
-    waveTables.map(async (table, i) =>
-      base(table)
+    WAVE_INFO.map(async ({ tableId }, i) =>
+      base(tableId)
         .select({ sort: [{ field: "First Name", direction: "asc" }] })
         .eachPage((records, fetchNextPage) => {
-          waves[i].push(
-            ...records.map(({ id, fields }) => ({ id, ...fields }))
+          mentors.push(
+            ...records.map(({ id, fields }) => formatMentor(id, i, fields))
           );
           fetchNextPage();
         })
     )
   );
 
-  return JSON.stringify({ waves });
+  return JSON.stringify({ mentors });
 };
