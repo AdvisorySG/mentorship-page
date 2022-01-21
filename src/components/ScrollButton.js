b36404d271dcb4c03cd74172146c3c6058fef674
IvanFengJK <ivanfengjunkai1997114@gmail.com>
2022-01-21 15:42:34 +0800

    back to top with white fill

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -7,7 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) => ({
   toTop: {
-      zIndex: 2,
+      zIndex: 2.5,
       position: 'fixed',
       bottom: '2vh',
       backgroundColor: '#FF8C00',
