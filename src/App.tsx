236be4e65ee2f25d015ae25e43ae5f1e1eff22af
florallam <55974415+florallam@users.noreply.github.com>
2022-03-26 11:13:27 +0800

    Add Role & Course of Study filters (#411)

--- src/App.tsx
+++ src/App.tsx
@@ -87,6 +87,8 @@ const configurationOptions = {
     // 3. Facet by scores, genre, publisher, and platform, which we'll use to build filters later.
     facets: {
       industries: { type: "value", size: 100 },
+      course_of_study: { type: "value", size: 100 },
+      role: { type: "value", size: 100 },
     },
   },
 };
@@ -140,6 +142,8 @@ function App() {
                       ]}
                     />
                     <Facet field="industries" label="Industries" />
+                    <Facet field="course_of_study" label="Course of Study" />
+                    <Facet field="role" label="Role" />
                   </div>
                 }
                 bodyHeader={
