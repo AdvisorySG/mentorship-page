3f924680e790bf508c56ff3191838899733b0c11
florallam <floralam73@gmail.com>
2022-04-11 03:26:25 +0800

    made changes to layout

--- src/ResultView.tsx
+++ src/ResultView.tsx
@@ -1,167 +1,100 @@
 import React from "react";
-import useMediaQuery from "@mui/material/useMediaQuery";
 
-const ResultBody = ({ body }: { body: any }) => {
-  const { industries, role, organisation, school, course_of_study, full_bio } =
-    body;
-  return (
-    <ul className="sui-result__details">
-      <li>
-        <span className="sui-result__label">Industries: </span>
-        <span
-          className="sui-result__value"
-          dangerouslySetInnerHTML={{
-            __html:
-              industries && industries.raw ? industries.raw.join(", ") : "NA",
-          }}
-        />
-      </li>
-      <li>
-        <span className="sui-result__label">Role: </span>
-        <span
-          className="sui-result__value"
-          dangerouslySetInnerHTML={{
-            __html: role && role.snippet ? role.snippet : "NA",
-          }}
-        />
-      </li>
-      <li>
-        <span className="sui-result__label">Organisation: </span>
-        <span
-          className="sui-result__value"
-          dangerouslySetInnerHTML={{
-            __html:
-              organisation && organisation.snippet
-                ? organisation.snippet
-                : "NA",
-          }}
-        />
-      </li>
-      <li>
-        <span className="sui-result__label">School graduated from: </span>
-        <span
-          className="sui-result__value"
-          dangerouslySetInnerHTML={{
-            __html: school && school.snippet ? school.snippet : "NA",
-          }}
-        />
-      </li>
-      <li>
-        <span className="sui-result__label">Course of study: </span>
-        <span
-          className="sui-result__value"
-          dangerouslySetInnerHTML={{
-            __html:
-              course_of_study && course_of_study.snippet
-                ? course_of_study.snippet
-                : "NA",
-          }}
-        />
-      </li>
-      <li>
-        <span className="sui-result__label">Biography: </span>
-        <span
-          className="sui-result__value"
-          dangerouslySetInnerHTML={{
-            __html:
-              full_bio && full_bio.snippet ? full_bio.snippet + "..." : "NA",
-          }}
-        />
-      </li>
-    </ul>
-  );
-};
-
-const ResultTitleMobile = ({ title }: { title: any }) => {
-  const { name } = title;
-  return (
-    <span
-      className="sui-result__title"
-      dangerouslySetInnerHTML={{
-        __html: name && name.snippet ? name.snippet : "NA",
-      }}
-      style={{ textAlign: "center", width: "100%" }}
-    />
-  );
-};
-
-const ResultTitle = ({ title }: { title: any }) => {
-  const { name } = title;
-  return (
-    <span
-      className="sui-result__title"
-      dangerouslySetInnerHTML={{
-        __html: name && name.snippet ? name.snippet : "NA",
-      }}
-    />
-  );
-};
-
+import "./ResultView.css";
 const ResultView = ({ result }: { result: any }) => {
-  const MobileView = useMediaQuery("(max-width:800px)");
-  if (MobileView) {
-    return (
-      <div>
-        <li className="sui-result">
-          <div className="sui-result__header">
-            <ResultTitleMobile title={result} />
-          </div>
-          <div
-            className="sui-result__image"
-            style={{
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
-              paddingLeft: "0px",
-              paddingTop: "0px",
-            }}
-          >
-            <img
-              src={result.thumbnail_image_url.raw}
-              alt="thumb"
-              style={{
-                display: "block",
-                objectFit: "cover",
-                objectPosition: "center",
-                paddingLeft: "-24px",
-              }}
-              className="mentor-image"
-            />
-          </div>
-          <div className="sui-result__body">
-            <ResultBody body={result} />
-          </div>
-        </li>
-      </div>
-    );
-  }
+  const {
+    course_of_study,
+    full_bio,
+    industries,
+    name,
+    organisation,
+    role,
+    school,
+  } = result;
+
   return (
     <div>
       <li className="sui-result">
         <div className="sui-result__header">
-          <ResultTitle title={result} />
+          <span
+            className="sui-result__title"
+            dangerouslySetInnerHTML={{
+              __html: name && name.snippet ? name.snippet : "NA",
+            }}
+          />
         </div>
         <div className="sui-result__body">
-          <div
-            className="sui-result__image"
-            style={{
-              maxWidth: "140px",
-              paddingLeft: "24px",
-              paddingTop: "10px",
-            }}
-          >
-            <img
-              src={result.thumbnail_image_url.raw}
-              alt="thumb"
-              style={{
-                display: "block",
-                objectFit: "cover",
-                objectPosition: "center",
-              }}
-              className="mentor-image"
-            />
+          <div className="sui-result__image">
+            <img src={result.thumbnail_image_url.raw} />
           </div>
-          <ResultBody body={result} />
+          <ul className="sui-result__details">
+            <li>
+              <span className="sui-result__label">Industries: </span>
+              <span
+                className="sui-result__value"
+                dangerouslySetInnerHTML={{
+                  __html:
+                    industries && industries.raw
+                      ? industries.raw.join(", ")
+                      : "NA",
+                }}
+              />
+            </li>
+            <li>
+              <span className="sui-result__label">Role: </span>
+              <span
+                className="sui-result__value"
+                dangerouslySetInnerHTML={{
+                  __html: role && role.snippet ? role.snippet : "NA",
+                }}
+              />
+            </li>
+            <li>
+              <span className="sui-result__label">Organisation: </span>
+              <span
+                className="sui-result__value"
+                dangerouslySetInnerHTML={{
+                  __html:
+                    organisation && organisation.snippet
+                      ? organisation.snippet
+                      : "NA",
+                }}
+              />
+            </li>
+            <li>
+              <span className="sui-result__label">School graduated from: </span>
+              <span
+                className="sui-result__value"
+                dangerouslySetInnerHTML={{
+                  __html: school && school.snippet ? school.snippet : "NA",
+                }}
+              />
+            </li>
+            <li>
+              <span className="sui-result__label">Course of study: </span>
+              <span
+                className="sui-result__value"
+                dangerouslySetInnerHTML={{
+                  __html:
+                    course_of_study && course_of_study.snippet
+                      ? course_of_study.snippet
+                      : "NA",
+                }}
+              />
+            </li>
+            <li>
+              <span className="sui-result__label">Biography: </span>
+              <span
+                className="sui-result__value"
+                dangerouslySetInnerHTML={{
+                  __html:
+                    full_bio && full_bio.snippet
+                      ? full_bio.snippet + "..."
+                      : "NA",
+                }}
+              />
+            </li>
+          </ul>
         </div>
       </li>
     </div>
