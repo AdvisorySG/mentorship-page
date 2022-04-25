b601f9e096174855355ffd843212a3110414fee4
Ng Wei En <weien1292@gmail.com>
2022-04-25 10:31:41 +0800

    Header: Update wave logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,3 @@
 export const advisoryLogo = `${process.env.PUBLIC_URL}/advisory-logo.png`;
-export const advisoryMentorshipLogo = `${process.env.PUBLIC_URL}/advisory-mentorship-logo-wave2.png`;
+export const advisoryMentorshipLogo = `${process.env.PUBLIC_URL}/advisory-mentorship-logo-2022.png`;
 export const advisoryMentorshipPartners = `${process.env.PUBLIC_URL}/advisory-mentorship-partners.png`;
