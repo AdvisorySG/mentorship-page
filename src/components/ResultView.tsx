788a0c8bbb62dcc3fd1746e0c9d18454ccf084c1
Ng Wei En <weien1292@gmail.com>
2022-05-02 14:47:17 +0800

    ResultView: Hotfix for various issues
    1. Profile image is not sized consistently when card is shorter
    2. More permanent fix to missing brackets issue, which does not address
       other punctuation marks, e.g. "Corporate M&A"
    3. Deal with missing role/organisation and other fields

--- src/components/ResultView.tsx
+++ src/components/ResultView.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
 import { Chip } from "@mui/material";
 import {
@@ -20,29 +20,13 @@ import { htmlToText } from "html-to-text";
 
 import "./ResultView.css";
 
-// This function is needed as ElasticSearch trims off closed brackets at the
-// end of snippets. Please refer to the following issue:
-// https://github.com/AdvisorySG/mentorship-page/issues/440
-const fillMissing = (snippet: string) => {
-  const str = String(snippet);
-  let open = 0;
-  let close = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str.charAt(i) === "(") {
-      open += 1;
-    } else if (str.charAt(i) === ")") {
-      close += 1;
-    }
-  }
-  if (open - close === 1) {
-    snippet = snippet + ")";
-  }
-  return snippet;
-};
-
 // Performs intelligent snippet truncation by removing leading/trailing periods and
 // whitespace, and filling with ellipsis accordingly.
-const fillEllipsis = (snippet: string, full: string) => {
+const fillEllipsis = (snippet: string | null, full: string) => {
+  if (!snippet) {
+    return full;
+  }
+
   const snippetRaw = htmlToText(snippet, { wordwrap: false });
 
   const startRegex = /^[\s.!?]+/g;
@@ -72,7 +56,11 @@ const fillEllipsis = (snippet: string, full: string) => {
 };
 
 // Fills in highlights from snippet text.
-const fillHighlights = (snippet: string, full: string) => {
+const fillHighlights = (snippet: string | null, full: string): string => {
+  if (!snippet) {
+    return full;
+  }
+
   const snippetRaw = htmlToText(snippet, { wordwrap: false });
   return full.replace(snippetRaw, snippet);
 };
@@ -105,28 +93,30 @@ const ResultView = ({ result }: { result: any }) => {
     school,
   } = result;
 
+  console.log(industries);
+
   const displayCourseOfStudy =
-    courseOfStudy && courseOfStudy.snippet
-      ? fillMissing(courseOfStudy.snippet)
-      : "NA";
+    courseOfStudy && courseOfStudy.raw
+      ? fillHighlights(courseOfStudy.snippet, courseOfStudy.raw)
+      : null;
   const displayShortBio =
-    fullBio && fullBio.snippet && fullBio.raw
-      ? fillEllipsis(fullBio.snippet, fullBio.raw)
-      : "NA";
+    fullBio && fullBio.raw ? fillEllipsis(fullBio.snippet, fullBio.raw) : null;
   const displayFullBio =
-    fullBio && fullBio.snippet && fullBio.raw
+    fullBio && fullBio.raw
       ? fillHighlights(fullBio.snippet, fullBio.raw)
-      : "NA";
+      : null;
   const displayIndustries =
     industries && Array.isArray(industries.raw) ? industries.raw : [];
-  const displayName = name && name.snippet ? fillMissing(name.snippet) : "NA";
+  const displayName =
+    name && name.raw ? fillHighlights(name.snippet, name.raw) : null;
   const displayOrganisation =
-    organisation && organisation.snippet
-      ? fillMissing(organisation.snippet)
-      : "NA";
-  const displayRole = role && role.snippet ? fillMissing(role.snippet) : "NA";
+    organisation && organisation.raw
+      ? fillHighlights(organisation.snippet, organisation.raw)
+      : null;
+  const displayRole =
+    role && role.raw ? fillHighlights(role.snippet, role.raw) : null;
   const displaySchool =
-    school && school.snippet ? fillMissing(school.snippet) : "NA";
+    school && school.raw ? fillHighlights(school.snippet, school.raw) : null;
 
   displayIndustries.forEach((industry: string) => {
     if (!industryColors.has(industry)) {
@@ -140,14 +130,16 @@ const ResultView = ({ result }: { result: any }) => {
     <div>
       <li className="sui-result">
         <div className="sui-result__image">
-          <img src={result.thumbnail_image_url.raw} alt={displayName} />
+          <img src={result.thumbnail_image_url.raw} alt={displayName ?? ""} />
         </div>
         <div className="sui-result__body">
           <div className="sui-result__header">
-            <span
-              className="sui-result__title"
-              dangerouslySetInnerHTML={{ __html: displayName }}
-            />
+            {displayName && (
+              <span
+                className="sui-result__title"
+                dangerouslySetInnerHTML={{ __html: displayName }}
+              />
+            )}
           </div>
           <ul className="sui-result__details">
             <li className="sui-result__industries">
@@ -164,20 +156,27 @@ const ResultView = ({ result }: { result: any }) => {
                 />
               ))}
             </li>
-            <li>
-              <span
-                className="sui-result__value"
-                style={{ fontSize: 16, fontWeight: 600 }}
-                dangerouslySetInnerHTML={{ __html: displayRole }}
-              />
-              <span> at </span>
-              <span
-                className="sui-result__value"
-                style={{ fontSize: 16, fontWeight: 600 }}
-                dangerouslySetInnerHTML={{ __html: displayOrganisation }}
-              />
-            </li>
-            {displayCourseOfStudy !== "NA" && (
+            {displayRole && (
+              <li>
+                <span
+                  className="sui-result__value"
+                  style={{ fontSize: 16, fontWeight: 600 }}
+                  dangerouslySetInnerHTML={{ __html: displayRole }}
+                />
+                {displayOrganisation && (
+                  <Fragment>
+                    {" "}
+                    <span> at </span>
+                    <span
+                      className="sui-result__value"
+                      style={{ fontSize: 16, fontWeight: 600 }}
+                      dangerouslySetInnerHTML={{ __html: displayOrganisation }}
+                    />
+                  </Fragment>
+                )}
+              </li>
+            )}
+            {displayCourseOfStudy && (
               <li>
                 <span className="sui-result__label">Studied </span>
                 <span
@@ -187,7 +186,7 @@ const ResultView = ({ result }: { result: any }) => {
                 />
               </li>
             )}
-            {displaySchool !== "NA" && (
+            {displaySchool && (
               <li>
                 <span className="sui-result__label">Graduated from </span>
                 <span
@@ -198,26 +197,28 @@ const ResultView = ({ result }: { result: any }) => {
               </li>
             )}
             <hr />
-            <li>
-              <span
-                className="sui-result__biography"
-                style={{ fontSize: 14 }}
-                dangerouslySetInnerHTML={{
-                  __html: readMore ? displayFullBio : displayShortBio,
-                }}
-              />
-              <a
-                className="sui-result__readMore"
-                style={{ fontSize: 14 }}
-                href="#readMore"
-                onClick={(e: any) => {
-                  e.preventDefault();
-                  setReadMore(!readMore);
-                }}
-              >
-                {readMore ? "Read Less" : "Read More"}
-              </a>
-            </li>
+            {displayFullBio && displayShortBio && (
+              <li>
+                <span
+                  className="sui-result__biography"
+                  style={{ fontSize: 14 }}
+                  dangerouslySetInnerHTML={{
+                    __html: readMore ? displayFullBio : displayShortBio,
+                  }}
+                />
+                <a
+                  className="sui-result__readMore"
+                  style={{ fontSize: 14 }}
+                  href="#readMore"
+                  onClick={(e: any) => {
+                    e.preventDefault();
+                    setReadMore(!readMore);
+                  }}
+                >
+                  {readMore ? "Read Less" : "Read More"}
+                </a>
+              </li>
+            )}
           </ul>
         </div>
       </li>
