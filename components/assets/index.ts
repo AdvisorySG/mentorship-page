2ee678eb8f4ec559219b328afe7ad887d89181c4
name <zunmun@gmail.com>
2023-01-09 18:04:47 +0800

    CSS Files Organisation

--- components/assets/index.ts
+++ components/assets/index.ts
@@ -1,3 +1,3 @@
-export const advisoryLogo = `${process.env.NEXT_PUBLIC_PUBLIC_URL}/advisory-logo.png`;
-export const advisoryMentorshipLogo = `${process.env.NEXT_PUBLIC_PUBLIC_URL}/advisory-mentorship-logo-2022.png`;
-export const advisoryMentorshipPartners = `${process.env.NEXT_PUBLIC_PUBLIC_URL}/advisory-mentorship-partners.png`;
+export const advisoryLogo = `/advisory-logo.png`;
+export const advisoryMentorshipLogo = `/advisory-mentorship-logo-2022.png`;
+export const advisoryMentorshipPartners = `/advisory-mentorship-partners.png`;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import "../styles/globals.css";
+import "../styles/index.css";
+import "../styles/App.css";
+import "../styles/Header.css";
+import "../styles/ResultView.css";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style>
+        {`
+      .sui-result + .sui-result {
+        margin-top: 0px;
+      }
+      `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
