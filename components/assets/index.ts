4a2c8d947e872899abef572444aa9fe1d9909126
Terence Chan Zun Mun <zunmun@gmail.com>
2023-07-22 05:11:59 +0800

    Home: update images for 2023 (#623)

--- components/assets/index.ts
+++ components/assets/index.ts
@@ -1,3 +1,3 @@
 export const advisoryLogo = `/advisory-logo.png`;
-export const advisoryMentorshipLogo = `/advisory-mentorship-logo-2022.png`;
+export const advisoryMentorshipLogo = `/advisory-mentorship-logo-2023.png`;
 export const advisoryMentorshipPartners = `/advisory-mentorship-partners.png`;
