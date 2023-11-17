e42ce1fd5746bf0039be946453486e83d85c1c4f
M. Allen Jeffrey <mpjallenjeffrey@gmail.com>
2023-11-17 18:17:00 +0800

    make Logos.tsx more concise

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,100 +1,56 @@
 import React from "react";
 import "../styles/Logo.css";
 
-import {
-  aisp,
-  avanade,
-  basf,
-  bcg,
-  bigIdea,
-  bloomberg,
-  citi,
-  csa,
-  dbs,
-  deloitte,
-  edb,
-  edelman,
-  ey,
-  gic,
-  govtech,
-  ipr,
-  kearney,
-  maxeon,
-  mha,
-  nac,
-  nvpc,
-  one,
-  sap,
-  sc,
-  sgtech,
-  ttsh,
-  we,
-  google,
-} from "../components/assets";
+import * as logos from "../components/assets";
 
 const Logo: React.FC = () => {
+  const logoArray = [
+    logos.aisp,
+    logos.avanade,
+    logos.basf,
+    logos.bcg,
+    logos.bigIdea,
+    logos.bloomberg,
+    logos.citi,
+    logos.csa,
+    logos.dbs,
+    logos.deloitte,
+    logos.edb,
+    logos.edelman,
+    logos.ey,
+    logos.gic,
+    logos.govtech,
+    logos.ipr,
+    logos.kearney,
+    logos.maxeon,
+    logos.mha,
+    logos.nac,
+    logos.nvpc,
+    logos.one,
+    logos.sap,
+    logos.sc,
+    logos.sgtech,
+    logos.ttsh,
+    logos.we,
+    logos.google,
+  ];
+
   return (
     <div className="logos">
       <div className="logos-slide">
-        <img src={aisp} alt="3m" />
-        <img src={avanade} alt="Barstool Store" />
-        <img src={basf} alt="Budweiser" />
-        <img src={bcg} alt="Buzzfeed" />
-        <img src={bigIdea} alt="Forbes" />
-        <img src={bloomberg} alt="Macys" />
-        <img src={citi} alt="Men's Health" />
-        <img src={csa} alt="3m" />
-        <img src={dbs} alt="Barstool Store" />
-        <img src={deloitte} alt="Budweiser" />
-        <img src={edb} alt="Buzzfeed" />
-        <img src={google} alt="Forbes" />
-        <img src={ey} alt="Macys" />
-        <img src={gic} alt="Men's Health" />
-        <img src={govtech} alt="3m" />
-        <img src={ipr} alt="Barstool Store" />
-        <img src={kearney} alt="Budweiser" />
-        <img src={maxeon} alt="Buzzfeed" />
-        <img src={mha} alt="Forbes" />
-        <img src={nac} alt="Macys" />
-        <img src={nvpc} alt="Men's Health" />
-        <img src={one} alt="3m" />
-        <img src={sap} alt="Barstool Store" />
-        <img src={sc} alt="Budweiser" />
-        <img src={sgtech} alt="Buzzfeed" />
-        <img src={ttsh} alt="Forbes" />
-        <img src={we} alt="Macys" />
-        <img src={edelman} alt="Men's Health" />
+        {logoArray.map((logo, index) => (
+          <img key={index} src={logo} alt={`Logo ${index + 1}`} />
+        ))}
       </div>
 
       <div className="logos-slide">
-        <img src={aisp} alt="3m" />
-        <img src={avanade} alt="Barstool Store" />
-        <img src={basf} alt="Budweiser" />
-        <img src={bcg} alt="Buzzfeed" />
-        <img src={bigIdea} alt="Forbes" />
-        <img src={bloomberg} alt="Macys" />
-        <img src={citi} alt="Men's Health" />
-        <img src={csa} alt="3m" />
-        <img src={dbs} alt="Barstool Store" />
-        <img src={deloitte} alt="Budweiser" />
-        <img src={edb} alt="Buzzfeed" />
-        <img src={google} alt="Forbes" />
-        <img src={ey} alt="Macys" />
-        <img src={gic} alt="Men's Health" />
-        <img src={govtech} alt="3m" />
-        <img src={ipr} alt="Barstool Store" />
-        <img src={kearney} alt="Budweiser" />
-        <img src={maxeon} alt="Buzzfeed" />
-        <img src={mha} alt="Forbes" />
-        <img src={nac} alt="Macys" />
-        <img src={nvpc} alt="Men's Health" />
-        <img src={one} alt="3m" />
-        <img src={sap} alt="Barstool Store" />
-        <img src={sc} alt="Budweiser" />
-        <img src={sgtech} alt="Buzzfeed" />
-        <img src={ttsh} alt="Forbes" />
-        <img src={we} alt="Macys" />
-        <img src={edelman} alt="Men's Health" />
+        {logoArray.map((logo, index) => (
+          <img
+            key={index + logoArray.length}
+            src={logo}
+            alt={`Logo ${index + 1}`}
+          />
+        ))}
       </div>
     </div>
   );
