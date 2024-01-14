2d2a332d5788a76da2f38a759647739d3863a5c7
Ng Wei En <weien1292@gmail.com>
2024-01-14 15:38:48 +0800

    feat(apply): impl table of resource links (#671)
    Co-authored-by: Yash Chellani <35882565+yashchellani@users.noreply.github.com>

--- pages/apply.tsx
+++ pages/apply.tsx
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
-
+import React from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Button from "@mui/material/Button";
-
+import Box from "@mui/material/Box";
+import { List, ListItem, Link } from "@mui/material";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import ResultView from "../components/ResultView";
 import "../styles/App.css";
+import Divider from "@mui/material/Divider";
+
+const links = [
+  {
+    text: "Application Guide on CVs and Resumes",
+    url: "https://drive.google.com/file/d/1w7zlMuGY_7hVXeW0LaNKXWWjLRt3ic4G/view?usp=sharing",
+  },
+  {
+    text: "FAQs for Applicants",
+    url: "https://docs.google.com/document/d/1mGjAsgTP7KIheWVDm-TZ5Hxxf8WYPfWemiqcZ40XCwo/edit?usp=sharing",
+  },
+  {
+    text: "Sample Application Response",
+    url: "https://drive.google.com/file/d/1aA3l_WQ1ehGO8UBKuRFLqj4URpQbAVzN/view?usp=sharing",
+  },
+  {
+    text: "Industries to Filter Mentors",
+    url: "https://drive.google.com/file/d/19lUr2BchbYFAPMjEbN_M2mf1pyw-n7pj/view?usp=sharing",
+  },
+];
 
 const App = () => {
   const isSmall = useMediaQuery("(max-width: 800px)");
+
   return (
     <div className="container">
       <div
@@ -33,36 +49,49 @@ const App = () => {
           <img
             style={{ width: "100%" }}
             src="https://images.template.net/wp-content/uploads/2017/01/16095358/Sample-Blank-Timeline.jpg"
+            alt="Timeline"
           />
         </div>
         <div>
-          <h2 style={{ color: "var(--brand-color)" }}>Resources</h2>
-          <div className="resources">
-            <Button
-              variant="text"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://drive.google.com/file/d/1w7zlMuGY_7hVXeW0LaNKXWWjLRt3ic4G/view?usp=sharing"
+          <h2>Resources</h2>
+          <Box textAlign="center">
+            <List
+              style={{
+                display: "flex",
+                flexDirection: isSmall ? "column" : "row",
+                justifyContent: "center",
+                padding: 0,
+                flexWrap: "wrap",
+              }}
             >
-              APPLICATION GUIDE ON CV AND RESUME
-            </Button>{" "}
-            <Button
-              variant="text"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://drive.google.com/file/d/1aA3l_WQ1ehGO8UBKuRFLqj4URpQbAVzN/view?usp=sharing"
-            >
-              SAMPLE APPLICATION RESPONSE
-            </Button>{" "}
-            <Button
-              variant="text"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://drive.google.com/file/d/19lUr2BchbYFAPMjEbN_M2mf1pyw-n7pj/view?usp=sharing"
-            >
-              VIEW LIST OF INDUSTRIES
-            </Button>
-          </div>
+              {links.map((link, index) => (
+                <React.Fragment key={index}>
+                  <ListItem
+                    component="li"
+                    onClick={() =>
+                      window.open(link.url, "_blank", "noopener noreferrer")
+                    }
+                    style={{
+                      listStyle: "none",
+                      margin: isSmall ? "0 0 10px 0" : "0 10px",
+                      width: isSmall ? "100%" : "auto",
+                      boxSizing: "border-box",
+                    }}
+                  >
+                    <Link
+                      href={link.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      underline="hover"
+                    >
+                      <Typography variant="body1">{link.text}</Typography>
+                    </Link>
+                  </ListItem>
+                  {index < links.length - 1 && <Divider />}
+                </React.Fragment>
+              ))}
+            </List>
+          </Box>
         </div>
         <div
           style={{
