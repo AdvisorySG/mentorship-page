67210b2b9147e67f01954f4fdc42c6fcf30aa6f9
Unknownflow <81907539+Unknownflow@users.noreply.github.com>
2024-03-10 12:30:15 +0000

    changed margin when screen is small from 20% to 12.5%

--- pages/index.tsx
+++ pages/index.tsx
@@ -344,7 +344,7 @@ const Index = () => {
                 fontSize: "24px",
                 color: "var(--brand-color)",
                 margin: "10px",
-                marginLeft: isSmall ? "20%" : "25%",
+                marginLeft: isSmall ? "12.5%" : "25%",
                 position: "absolute",
               }}
             >
@@ -360,7 +360,7 @@ const Index = () => {
                 color: "var(--brand-color)",
                 margin: "10px",
                 position: "absolute",
-                marginRight: isSmall ? "20%" : "25%",
+                marginRight: isSmall ? "12.5%" : "25%",
               }}
             >
               {">"}
