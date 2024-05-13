9ceefac9bf8808b4fbe7b63a9c4016b6bc64c18d
IvanFengJK <ivanfengjunkai1997114@gmail.com>
2024-05-13 17:56:31 +0800

    fix: Merge conflict

--- components/PrivacyNote.tsx
+++ components/PrivacyNote.tsx
@@ -17,12 +17,8 @@ const style = {
   width: "80%",
   maxWidth: "600px",
   bgcolor: "background.paper",
-<<<<<<< HEAD
   border: "none",
   outline: "none",
-=======
-  border: "2px solid #000",
->>>>>>> c9ee53d (feat: new privacy modal based on MPT-53)
   boxShadow: 24,
   p: 4,
 };
@@ -48,10 +44,6 @@ function PrivacyModal() {
   return (
     <Modal
       open={isOpen}
-<<<<<<< HEAD
-=======
-      onClose={handleAgree}
->>>>>>> c9ee53d (feat: new privacy modal based on MPT-53)
       aria-labelledby="privacy-policy-title"
       aria-describedby="privacy-policy-description"
     >
