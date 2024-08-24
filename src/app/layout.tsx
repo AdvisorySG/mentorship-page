e2196571858ec1a1b8b2c8c9c6ff2485a87db81e
dabby9734 <62941997+dabby9734@users.noreply.github.com>
2024-08-24 16:58:09 +0800

    chore: migrate pages/_layout.js and pages/_app.js to app/layout.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,69 @@
+import type { Metadata, Viewport } from "next";
+import Script from "next/script";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <Script
+        async
+        src="https://umami.advisory.sg/script.js"
+        data-website-id="2f694eab-769e-420a-bb31-4efcb3662bf5"
+      />
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
+
+export const viewport: Viewport = {
+  // See https://nextjs.org/docs/app/api-reference/functions/generate-viewport for configuration info
+  width: "device-width",
+  initialScale: 1,
+  themeColor: "#000000",
+};
+
+export const metadata: Metadata = {
+  // See https://nextjs.org/docs/app/api-reference/functions/generate-metadata for configuration info
+  title: "Advisory Mentorship Programme",
+  description: "The Advisory Mentorship Programme",
+
+  // Icons
+  icons: {
+    icon: [{ url: "/favicon.ico" }],
+    apple: [{ url: "/logo192.png" }],
+  },
+
+  // Manifest
+  manifest: "/manifest.json",
+
+  // TODO: Add Google metadata
+  // The following needs to be added to the HTML <head> somehow:
+  // <meta itemProp="name" content="Advisory Mentorship Programme" />
+  // <meta
+  //     itemProp="description"
+  //     content="The Advisory Mentorship Programme"
+  // />
+  // <meta itemProp="image" content="/advisory-mentorship-logo.jpg" />
+
+  // Facebook / OpenGraph
+  openGraph: {
+    url: "https://mentorship.advisory.sg",
+    type: "website",
+    title: "Advisory Mentorship Programme",
+    description: "The Advisory Mentorship Programme",
+    images: [{ url: "/advisory-mentorship-logo.jpg" }],
+  },
+
+  // Twitter
+  twitter: {
+    card: "summary_large_image",
+    title: "Advisory Mentorship Programme",
+    description: "The Advisory Mentorship Programme",
+    images: [{ url: "/advisory-mentorship-logo.jpg" }],
+  },
+};
