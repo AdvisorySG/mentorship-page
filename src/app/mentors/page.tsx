8f91b879a4f17f8b268e234b9c0ae33f7ecfa303
Clarence <81907539+Unknownflow@users.noreply.github.com>
2025-04-05 23:16:00 +0800

    add behavioral analytics

--- src/app/mentors/page.tsx
+++ src/app/mentors/page.tsx
@@ -20,6 +20,20 @@ import ResultView from "../../components/ResultView";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
 import "../../styles/App.css";
 
+import {
+  createTracker,
+  trackPageView,
+  trackSearch,
+  trackSearchClick
+} from "@elastic/behavioral-analytics-javascript-tracker";
+import AnalyticsPlugin from "@elastic/search-ui-analytics-plugin";
+
+createTracker({
+  endpoint: process.env.TRACKER_END_POINT,
+  collectionName: process.env.NODE_ENV,
+  apiKey: process.env.API_KEY
+});
+
 const ELASTIC_CLOUD_ID =
   "My_deployment:YXAtc291dGhlYXN0LTEuYXdzLmZvdW5kLmlvJGZmM2E4NzcwZmM2YzRiYTZiMDcwZmZiNzQzM2ExMDk0JDgwZDc3ZGY2NGQxODQwMjNiNDkxOWQ0YWEwNWVjZjRm";
 const ELASTIC_APIKEY =
@@ -83,6 +97,7 @@ const App = () => {
         "course_of_study.keyword": { type: "value", size: 100 },
       },
     },
+    plugins: [ AnalyticsPlugin() ]
   };
 
   return (
