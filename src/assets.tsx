992b16b5357b8c6cadc14229230545a1466165d6
Ng Wei En <2070423+wei2912@users.noreply.github.com>
2025-05-17 23:11:18 +0800

    fix(PartnerLogos): add (some) 2025 logos

--- src/assets.tsx
+++ src/assets.tsx
@@ -3,22 +3,19 @@ export const advisoryMentorshipLogo = "/advisory-mentorship-logo.png";
 
 export const partnerLogos = [
   ["AiSP", "/logos/01_AiSP.png"],
-  ["Avanade", "/logos/02_Avanade.png"],
+  ["Allen & Glendhill", "/logos/02_Allen_Glendhill.png"],
   ["BASF", "/logos/03_BASF.png"],
   ["BCG", "/logos/04_BCG.png"],
   ["BCG X", "/logos/05_BCG X.png"],
-  ["CAG", "/logos/06_CAG.png"],
   ["Citi", "/logos/07_Citi.png"],
   ["ECDA", "/logos/09_ECDA.png"],
   ["EY", "/logos/11_EY.png"],
-  ["Forward Dyson", "/logos/12_Forward Dyson.png"],
+  ["Forward Dyson", "/logos/12_Forward_Dyson.png"],
   ["GIC", "/logos/13_GIC.png"],
-  ["GovTech", "/logos/14_GovTech.png"],
   ["IMDA", "/logos/15_IMDA.png"],
-  ["Maxeon", "/logos/16_Maxeon.png"],
   ["MHA", "/logos/17_MHA.png"],
-  ["NVPC", "/logos/18_NVPC.png"],
-  ["Standard Chartered", "/logos/19_Standard Chartered.png"],
+  ["ONE", "/logos/18_ONE.png"],
+  ["Standard Chartered", "/logos/19_Standard_Chartered.png"],
 ];
 
 export const footerLogo = "/footer-logo.png";
