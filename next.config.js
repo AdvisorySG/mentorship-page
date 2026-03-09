d26c195903edf5d08829819ab69a86ce1f114cc4
Ng Wei En <2070423+wei2912@users.noreply.github.com>
2026-03-09 19:15:51 +0800

    build: upgrade to Next v15, React v19, Node v22 + devcontainer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const path = require("path");
-
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
@@ -9,33 +7,6 @@ const nextConfig = {
   // FIXME: get rid of this before launch!
   typescript: {
     ignoreBuildErrors: true,
-  },
-
-  // Disable css--modules component styling
-
-  // Source: https://cwtuan.blogspot.com/2022/10/disable-css-module-in-nextjs-v1231-sept.html
-
-  webpack(config) {
-    config.module.rules.forEach((rule) => {
-      const { oneOf } = rule;
-
-      if (oneOf) {
-        oneOf.forEach((one) => {
-          if (!`${one.issuer?.and}`.includes("_app")) return;
-
-          one.issuer.and = [path.resolve(__dirname)];
-        });
-      }
-    });
-
-    // see https://huggingface.co/docs/transformers.js/en/tutorials/next#step-2-install-and-configure-transformersjs
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      sharp$: false,
-      "onnxruntime-node$": false,
-    };
-
-    return config;
   },
 };
 
