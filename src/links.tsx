57f379103c2915b3c9346287142dcd2c27631387
Ng Wei En <2070423+wei2912@users.noreply.github.com>
2026-05-27 21:40:52 +0800

    chore(links): update Apply Now link

--- src/links.tsx
+++ src/links.tsx
@@ -1,5 +1,5 @@
 export const URL_APPLY =
-  "https://advisorysg.typeform.com/to/AXiefw7D#source=mentorsite";
+  "https://advisorysg.typeform.com/to/qB5MGnW1#source=mentorsite";
 export const URL_HOME = "/";
 export const URL_MAIN = "https://advisory.sg/";
 export const URL_FAQ = "/faq";
