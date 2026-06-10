6277e3afcbfa6bd07628d29c41a602e74d8c4fa9
Ng Wei En <2070423+wei2912@users.noreply.github.com>
2026-06-10 17:36:34 +0800

    feat: update logos for 2026 wave (#1072)
    * feat: update logos
    
    * feat: update logos for 2026 wave
    
    * update IMDA logo
    
    * fix typo in A&G logo
    
    ---------
    
    Co-authored-by: Terence Chan Zun Mun <zunmun@gmail.com>

--- src/assets.tsx
+++ src/assets.tsx
@@ -3,19 +3,21 @@ export const advisoryMentorshipLogo = "/advisory-mentorship-logo.png";
 
 export const partnerLogos = [
   ["AiSP", "/logos/01_AiSP.png"],
-  ["Allen & Glendhill", "/logos/02_Allen_Glendhill.png"],
+  ["Allen & Gledhill", "/logos/02_Allen_Glendhill.png"],
   ["BASF", "/logos/03_BASF.png"],
   ["BCG", "/logos/04_BCG.png"],
   ["BCG X", "/logos/05_BCG_X.png"],
-  ["ECDA", "/logos/09_ECDA.png"],
-  ["EDB", "/logos/10_EDB.png"],
-  ["EY", "/logos/11_EY.png"],
-  ["Forward Dyson", "/logos/12_Forward_Dyson.png"],
-  ["GIC", "/logos/13_GIC.png"],
-  ["IMDA", "/logos/15_IMDA.png"],
-  ["MHA", "/logos/17_MHA.png"],
-  ["ONE", "/logos/18_ONE.png"],
-  ["Standard Chartered", "/logos/19_Standard_Chartered.png"],
+  ["ByteDance", "/logos/06_ByteDance.webp"],
+  ["Deloitte", "/logos/07_Deloitte.webp"],
+  ["Edelman", "/logos/08_Edelman.webp"],
+  ["EY", "/logos/09_EY.png"],
+  ["GIC", "/logos/10_GIC.png"],
+  ["IMDA", "/logos/11_IMDA.png"],
+  ["MHA", "/logos/12_MHA.png"],
+  ["NVPC", "/logos/13_NVPC.webp"],
+  ["ONE", "/logos/14_ONE.png"],
+  ["PRCA", "/logos/15_PRCA.webp"],
+  ["Standard Chartered", "/logos/16_Standard_Chartered.png"],
 ];
 
 export const footerLogo = "/footer-logo.png";
